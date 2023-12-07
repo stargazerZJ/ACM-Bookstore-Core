91651713a214ef703eb23fd4df7f5cc6a16e375a
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-12-07 12:53:14 +0800

    pretest: change hash seed back

--- src/external_hash_map.cpp
+++ src/external_hash_map.cpp
@@ -14,7 +14,7 @@ Hash_t Hash::splitmix64(Hash_t x) {
 }
 Hash_t Hash::operator()(const std::string &str) {
   /// compute the hash every 8 characters to increase the speed
-  Hash_t hash = 0x9e3779b97f4a7c15;
+  Hash_t hash = 0;
   unsigned int i = 0;
   for (; i + 8 <= str.size(); i += 8) {
     hash = splitmix64(hash ^ *reinterpret_cast<const Hash_t *>(str.data() + i));
