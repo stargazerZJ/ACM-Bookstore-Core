4a4a08e3bcffb4f82fa240d076722e68a4688d54
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-12-09 10:34:17 +0800

    Implement and document the FinanceLog class

--- src/log.cpp
+++ src/log.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "log.h"
+FinanceRecord &FinanceRecord::log(unsigned long long int money) {
+  if (money > 0) income_sum_ += money;
+  else expenditure_sum_ += money;
+  return *this;
+}
+FinanceRecord FinanceRecord::operator-(const FinanceRecord &rhs) const {
+  return {income_sum_ - rhs.income_sum_, expenditure_sum_ - rhs.expenditure_sum_};
+}
+void FinanceLog::initialize(bool reset) {
+  log_.initialize(reset);
+  if (reset) {
+    current_sum_ = {0, 0};
+    log_.insert(current_sum_);
+  } else {
+    current_sum_ = log_.get(log_.size());
+  }
+}
+void FinanceLog::log(long long int money) {
+  current_sum_.log(money);
+  log_.insert(current_sum_);
+}
+FinanceRecord FinanceLog::sum(unsigned int count) {
+  if (count > log_.size() - 1) return {static_cast<unsigned long long>(-1), static_cast<unsigned long long>(-1)};
+  FinanceRecord record = log_.get(log_.size() - count);
+  return current_sum_ - record;
+}
+FinanceRecord FinanceLog::sum() {
+  return current_sum_;
+}
+void UserLog::initialize(bool reset) {
+  if (reset) {
+    log_.open(file_path_, std::ios::in | std::ios::out | std::ios::binary | std::ios::trunc);
+  } else {
+    log_.open(file_path_, std::ios::in | std::ios::out | std::ios::binary);
+  }
+  if (!log_.is_open()) {
+    throw std::runtime_error("Cannot open file " + file_path_);
+  }
+}
--- src/log.h
+++ src/log.h
@@ -5,6 +5,8 @@
 #ifndef BOOKSTORE_SRC_LOG_H_
 #define BOOKSTORE_SRC_LOG_H_
 
+#include "external_memory.h"
+
 enum class kExceptionType {
   K_SUCCESS,
   K_INVALID_COMMAND,
@@ -22,8 +24,69 @@ enum class kExceptionType {
   K_NOT_ENOUGH_LOGS
 };
 
-class FinanceLog {};
+/// @brief The finance record
+class FinanceRecord {
+  unsigned long long int income_sum_ = 0; // in cents
+  unsigned long long int expenditure_sum_ = 0; // in cents
+ public:
+  FinanceRecord() = default;
+  static constexpr unsigned int byte_size() { return sizeof(unsigned long long int) * 2; }
+  void toBytes(char *dest) const { std::memcpy(dest, this, byte_size()); }
+  void fromBytes(const char *src) { std::memcpy(this, src, byte_size()); }
+  explicit FinanceRecord(const char *bytes) { fromBytes(bytes); }
+  FinanceRecord(unsigned long long int income_sum, unsigned long long int expenditure_sum)
+      : income_sum_(income_sum), expenditure_sum_(expenditure_sum) {}
+  [[nodiscard]] unsigned long long int income_sum() const { return income_sum_; }
+  [[nodiscard]] unsigned long long int expenditure_sum() const { return expenditure_sum_; }
+  [[nodiscard]] unsigned long long int balance() const { return income_sum_ - expenditure_sum_; }
+  FinanceRecord operator-(const FinanceRecord &rhs) const;
+  /// @brief Log a transaction
+  /// @param money The amount of money
+  /// @details If `money` is positive, it is regarded as income, otherwise it is regarded as expenditure.
+  /// @return FinanceRecord& The reference of the current FinanceRecord
+  FinanceRecord &log(unsigned long long int money);
+};
 
-class UserLog {};
+/**
+ * @brief The finance log
+ * @details The finance log is used to record the income and expenditure of the store.
+ * @details The finance log is stored in external memory.
+ * @attention `initialize` must be called before using the finance log.
+ */
+class FinanceLog {
+ private:
+  const std::string file_path_;
+  external_memory::List<FinanceRecord, false> log_;
+  FinanceRecord current_sum_ = {0, 0};
+ public:
+  /// \brief Construct a new FinanceLog object
+  explicit FinanceLog(std::string file_path = "finance") : file_path_(std::move(file_path)), log_(file_path_) {};
+  /// \brief Initialize the FinanceLog object
+  /// \param reset Whether to reset the FinanceLog object
+  /// \attention This function must be called before using any other functions.
+  /// \attention This function must not be called twice.
+  /// \attention If reset is true, all the information of the finance log will be lost.
+  void initialize(bool reset = false);
+  /// \brief Log a transaction
+  /// \param money The amount of money
+  /// @details If `money` is positive, it is regarded as income, otherwise it is regarded as expenditure.
+  void log(long long int money);
+  /// \brief Get the sum of the last `count` logs
+  /// \param count The number of logs
+  /// \return FinanceRecord The sum of the last `count` logs
+  /// \attention If `count` is larger than the number of logs, return FinanceRecord(-1, -1)
+  FinanceRecord sum(unsigned int count); // if `count` is larger than the number of logs, return FinanceRecord(-1, -1)
+  /// \brief Get the sum of all the logs
+  FinanceRecord sum();
+};
+
+class UserLog {
+ private:
+  const std::string file_path_;
+  std::fstream log_;
+ public:
+  explicit UserLog(std::string file_path = "log") : file_path_(std::move(file_path) + ".csv") {};
+  void initialize(bool reset = false);
+};
 
 #endif //BOOKSTORE_SRC_LOG_H_
