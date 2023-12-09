8bcbb056ce497798bbada75cb46ad09369842ac4
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-12-09 18:46:12 +0800

    Design, implement and document the BookStoreCLI class

--- src/cli.cpp
+++ src/cli.cpp
@@ -3,3 +3,203 @@
 //
 
 #include "cli.h"
+void BookStoreCLI::initialize(bool force_reset) {
+  book_store_.initialize(force_reset);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+}
+void BookStoreCLI::run() {
+  std::string line;
+  while (std::getline(is, line)) {
+    Command command(line);
+    const std::string &name = command.getName();
+    const Args &args = command.getArgs();
+    if (name.empty()) continue;
+    if (name == "exit" || name == "quit") {
+      return;
+    } else if (name == "su") {
+      runCommand(args, &BookStoreCLI::su);
+    } else if (name == "logout") {
+      runCommand(args, &BookStoreCLI::logout);
+    } else if (name == "register") {
+      runCommand(args, &BookStoreCLI::register_);
+    } else if (name == "passwd") {
+      runCommand(args, &BookStoreCLI::passwd);
+    } else if (name == "useradd") {
+      runCommand(args, &BookStoreCLI::useradd);
+    } else if (name == "delete") {
+      runCommand(args, &BookStoreCLI::delete_);
+    } else if (name == "show") {
+      if (!args.empty() && args[0] == "finance") runCommand(args, &BookStoreCLI::showFinance);
+      else runCommand(args, &BookStoreCLI::show);
+    } else if (name == "buy") {
+      runCommand(args, &BookStoreCLI::buy);
+    } else if (name == "select") {
+      runCommand(args, &BookStoreCLI::select);
+    } else if (name == "modify") {
+      runCommand(args, &BookStoreCLI::modify);
+    } else if (name == "import") {
+      runCommand(args, &BookStoreCLI::import_);
+    } else {
+      runCommand(args, &BookStoreCLI::invalidCommand);
+    }
+  }
+}
+void BookStoreCLI::runCommand(const BookStoreCLI::Args &args, Func func) {
+  kExceptionType ret = (this->*func)(args);
+  if (ret != kExceptionType::K_SUCCESS) {
+    os << "Invalid!" << endl;
+  }
+}
+kExceptionType BookStoreCLI::su(const BookStoreCLI::Args &args) {
+  if (args.empty() || args.size() > 2) return kExceptionType::K_INVALID_PARAMETER;
+  if (args.size() == 1) {
+    return book_store_.login(args[0], "");
+  } else {
+    return book_store_.login(args[0], args[1]);
+  }
+}
+kExceptionType BookStoreCLI::logout(const BookStoreCLI::Args &args) {
+  if (!args.empty()) return kExceptionType::K_INVALID_PARAMETER;
+  return book_store_.logout();
+}
+kExceptionType BookStoreCLI::register_(const BookStoreCLI::Args &args) {
+  if (args.size() != 3) return kExceptionType::K_INVALID_PARAMETER;
+  return book_store_.customerUseradd(args[0], args[1], args[2]);
+}
+kExceptionType BookStoreCLI::passwd(const BookStoreCLI::Args &args) {
+  if (args.size() != 2 && args.size() != 3) return kExceptionType::K_INVALID_PARAMETER;
+  if (args.size() == 2) {
+    return book_store_.passwd(args[0], args[1]);
+  } else {
+    return book_store_.passwd(args[0], args[2], args[1]);
+  }
+}
+kExceptionType BookStoreCLI::useradd(const BookStoreCLI::Args &args) {
+  if (args.size() != 4) return kExceptionType::K_INVALID_PARAMETER;
+  unsigned int privilege;
+  auto ret = Command::parseUnsignedInt(args[2]);
+  if (ret.first != kExceptionType::K_SUCCESS) return ret.first;
+  privilege = ret.second;
+  return book_store_.useradd(args[0], args[1], args[3], privilege);
+}
+kExceptionType BookStoreCLI::delete_(const BookStoreCLI::Args &args) {
+  if (args.size() != 1) return kExceptionType::K_INVALID_PARAMETER;
+  return book_store_.deluser(args[0]);
+}
+kExceptionType BookStoreCLI::show(const BookStoreCLI::Args &args) {
+  if (args.size() > 1) return kExceptionType::K_INVALID_PARAMETER;
+  Book params;
+  if (!args.empty()) {
+    auto ret = Command::parseFlag(args[0], false);
+    if (ret.first != kExceptionType::K_SUCCESS) return ret.first;
+    Command::Flag flag = ret.second;
+    if (flag.getFlag() == "ISBN") {
+      params.ISBN = flag.getValue();
+    } else if (flag.getFlag() == "name") {
+      auto result = Command::removeQuotationMarks(flag.getValue());
+      if (result.first != kExceptionType::K_SUCCESS) return result.first;
+      params.title = result.second;
+    } else if (flag.getFlag() == "author") {
+      auto result = Command::removeQuotationMarks(flag.getValue());
+      if (result.first != kExceptionType::K_SUCCESS) return result.first;
+      params.author = flag.getValue();
+    } else if (flag.getFlag() == "keyword") {
+      auto result = Command::removeQuotationMarks(flag.getValue());
+      if (result.first != kExceptionType::K_SUCCESS) return result.first;
+      params.keywords = flag.getValue();
+    } else {
+      return kExceptionType::K_INVALID_PARAMETER;
+    }
+  }
+  auto result = book_store_.search(params);
+  if (result.first != kExceptionType::K_SUCCESS) return result.first;
+  for (const auto &book : result.second) {
+    os << book.ISBN << "\t" << book.title << "\t" << book.author << "\t" << book.keywords << "\t" << book.price << "\t"
+       << book.quantity << endl;
+  }
+  return kExceptionType::K_SUCCESS;
+}
+kExceptionType BookStoreCLI::buy(const BookStoreCLI::Args &args) {
+  if (args.size() != 2) return kExceptionType::K_INVALID_PARAMETER;
+  auto ret = Command::parseUnsignedInt(args[1]);
+  if (ret.first != kExceptionType::K_SUCCESS) return ret.first;
+  auto result = book_store_.purchase(args[0], ret.second);
+  if (result.first != kExceptionType::K_SUCCESS) return result.first;
+  os << printMoney(result.second) << endl;
+}
+std::string BookStoreCLI::printMoney(unsigned long long int money) {
+  std::ostringstream oss;
+  oss << money / 100 << "." << std::setw(2) << std::setfill('0') << money % 100;
+  return oss.str();
+}
+kExceptionType BookStoreCLI::select(const BookStoreCLI::Args &args) {
+  if (args.size() != 1) return kExceptionType::K_INVALID_PARAMETER;
+  return book_store_.select(args[0]);
+}
+kExceptionType BookStoreCLI::modify(const BookStoreCLI::Args &args) {
+  if (args.empty()) return kExceptionType::K_INVALID_PARAMETER;
+  Book new_book;
+  new_book.price = -1;
+  for (auto &flag_str : args) {
+    auto ret = Command::parseFlag(flag_str, false);
+    if (ret.first != kExceptionType::K_SUCCESS) return ret.first;
+    Command::Flag flag = ret.second;
+    if (flag.getFlag() == "ISBN") {
+      if (!new_book.ISBN.empty()) return kExceptionType::K_INVALID_PARAMETER;
+      new_book.ISBN = flag.getValue();
+    } else if (flag.getFlag() == "name") {
+      if (!new_book.title.empty()) return kExceptionType::K_INVALID_PARAMETER;
+      auto result = Command::removeQuotationMarks(flag.getValue());
+      if (result.first != kExceptionType::K_SUCCESS) return result.first;
+      new_book.title = result.second;
+    } else if (flag.getFlag() == "author") {
+      if (!new_book.author.empty()) return kExceptionType::K_INVALID_PARAMETER;
+      auto result = Command::removeQuotationMarks(flag.getValue());
+      if (result.first != kExceptionType::K_SUCCESS) return result.first;
+      new_book.author = flag.getValue();
+    } else if (flag.getFlag() == "keyword") {
+      if (!new_book.keywords.empty()) return kExceptionType::K_INVALID_PARAMETER;
+      auto result = Command::removeQuotationMarks(flag.getValue());
+      if (result.first != kExceptionType::K_SUCCESS) return result.first;
+      new_book.keywords = flag.getValue();
+    } else if (flag.getFlag() == "price") {
+      if (new_book.price != -1) return kExceptionType::K_INVALID_PARAMETER;
+      auto result = Command::parseMoney(flag.getValue());
+      if (result.first != kExceptionType::K_SUCCESS) return result.first;
+      new_book.price = result.second;
+    } else {
+      return kExceptionType::K_INVALID_PARAMETER;
+    }
+  }
+  return book_store_.modify(std::move(new_book));
+}
+kExceptionType BookStoreCLI::import_(const BookStoreCLI::Args &args) {
+  if (args.size() != 1) return kExceptionType::K_INVALID_PARAMETER;
+  auto ret = Command::parseUnsignedInt(args[0]);
+  if (ret.first != kExceptionType::K_SUCCESS) return ret.first;
+  auto result = book_store_.import_(ret.second);
+  if (result.first != kExceptionType::K_SUCCESS) return result.first;
+  os << printMoney(result.second) << endl;
+}
+kExceptionType BookStoreCLI::showFinance(const BookStoreCLI::Args &args) {
+  if (args.size() > 1) return kExceptionType::K_INVALID_PARAMETER;
+  FinanceRecord record;
+  if (!args.empty()) {
+    auto ret = Command::parseUnsignedInt(args[0]);
+    if (ret.first != kExceptionType::K_SUCCESS) return ret.first;
+    auto result = book_store_.showFinance(ret.second);
+    if (result.first != kExceptionType::K_SUCCESS) return result.first;
+    record = result.second;
+  } else {
+    auto result = book_store_.showFinance();
+    if (result.first != kExceptionType::K_SUCCESS) return result.first;
+    record = result.second;
+  }
+  os << "+ " << printMoney(record.income()) << " - " << printMoney(record.expenditure()) << endl;
+  return kExceptionType::K_SUCCESS;
+}
+kExceptionType BookStoreCLI::invalidCommand(const BookStoreCLI::Args &args) {
+  return kExceptionType::K_INVALID_COMMAND;
+}
--- src/cli.h
+++ src/cli.h
@@ -5,21 +5,68 @@
 #ifndef BOOKSTORE_SRC_CLI_H_
 #define BOOKSTORE_SRC_CLI_H_
 
+#include <iostream>
 #include "bookstore.h"
-#include "validator.h"
 #include "parser.h"
 
 class BookStoreCLI {
  private:
-  static constexpr char file_path[] = "bookstore";
+  const std::string file_path_;
+  BookStore book_store_;
+  std::istream &is = std::cin;
+  std::ostream &os = std::cout;
+  static inline constexpr char endl[] = "\n";
+  using Args = std::vector<std::string>;
+  using Func = kExceptionType (BookStoreCLI::*)(const Args &args);
+
+  static std::string printMoney(unsigned long long int money);
+  /// \brief Login
+  /// \details `su [UserID] ([Password])?`
+  kExceptionType su(const Args &args);
+  /// \brief Logout
+  /// \details `logout`
+  kExceptionType logout(const Args &args);
+  /// \brief Register
+  /// \details `register [UserID] [Password] [UserName]`
+  kExceptionType register_(const Args &args);
+  /// \brief Change the password of a user
+  /// \details `passwd [UserID] ([OldPassword])? [NewPassword]`
+  kExceptionType passwd(const Args &args);
+  /// \brief Add a new user
+  /// \details `useradd [UserID] [Password] [Privilege] [UserName]`
+  kExceptionType useradd(const Args &args);
+  /// \brief Delete a user
+  /// \details `delete [UserID]`
+  kExceptionType delete_(const Args &args);
+  /// \brief Search for books
+  /// \details `show (-ISBN=[ISBN]|-name="[BookName]"|-author="[Author]"|-keyword="[Keyword]")?`
+  kExceptionType show(const Args &args);
+  /// \brief Purchase books
+  /// \details `buy [ISBN] [Quantity]`
+  kExceptionType buy(const Args &args);
+  /// \brief Select a book
+  /// \details `select [ISBN]`
+  kExceptionType select(const Args &args);
+  /// \brief Modify the information of a book
+  /// \details `modify (-ISBN=[ISBN]|-name="[BookName]"|-author="[Author]"|-keyword="[Keyword]"|-price=[Price])+`
+  kExceptionType modify(const Args &args);
+  /// \brief Import books
+  /// \details `import [Quantity]`
+  kExceptionType import_(const Args &args);
+  /// \brief Show the finance log
+  /// \details `show finance ([Count])?`
+  kExceptionType showFinance(const Args &args);
+  /// \brief Invalid command
+  kExceptionType invalidCommand(const Args &args);
+  void runCommand(const Args &args, Func func);
  public:
-  BookStoreCLI() = default;
+  explicit BookStoreCLI(std::string file_path = "") : file_path_(std::move(file_path)) {}
 
   ~BookStoreCLI() = default;
 
-  bool initialize(bool force_reset = false);
+  void initialize(bool force_reset = false);
 
-  bool run();
+  void run();
 };
 
 #endif //BOOKSTORE_SRC_CLI_H_
